b60eeb141d45878f82046c1bd60c037f462b0a6d
geisheilia <gensheilia@gmail.com>
2023-02-14 14:29:54 +0800

    change num

--- src/main/pages/browser/lpOverview/index.tsx
+++ src/main/pages/browser/lpOverview/index.tsx
@@ -200,23 +200,26 @@ const ComponentBrowserCoinOverview: FC<{
     })
   }
   const nFormatter = (num:number, digits:number=1) => {
-    const si = [
-        { value: 1, symbol: "" },
-        { value: 1E3, symbol: "K" },
-        { value: 1E6, symbol: "M" },
-        { value: 1E9, symbol: "G" },
-        { value: 1E12, symbol: "T" },
-        { value: 1E15, symbol: "P" },
-        { value: 1E18, symbol: "E" }
-    ];
-    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-    let i;
-    for (i = si.length - 1; i > 0; i--) {
-        if (num >= si[i].value) {
-            break;
-        }
-    }
-    return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+    // const si = [
+    //     { value: 1, symbol: "" },
+    //     { value: 1E3, symbol: "K" },
+    //     { value: 1E6, symbol: "M" },
+    //     { value: 1E9, symbol: "G" },
+    //     { value: 1E12, symbol: "T" },
+    //     { value: 1E15, symbol: "P" },
+    //     { value: 1E18, symbol: "E" }
+    // ];
+    // const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    // let i;
+    // for (i = si.length - 1; i > 0; i--) {
+    //     if (num >= si[i].value) {
+    //         break;
+    //     }
+    // }
+    // return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+    let str = num.toString();
+    let reg = str.indexOf(".") > -1 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(?:\d{3})+$)/g;
+    return str.replace(reg,"$1,");
 }
 
   useEffect(() => {
@@ -293,7 +296,7 @@ const ComponentBrowserCoinOverview: FC<{
               <div className="overview-info-item-tips">
               {t('turnover')}
               </div>
-              ${nFormatter(Number(dataInfo.turnover?.num),3)} 
+              ${nFormatter(Number(dataInfo.turnover?.num))} 
               {/* <span className="rate">({dataInfo.turnover?.change}%)</span> */}
             </div>
 
